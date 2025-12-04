68db0f5708f46203708d18d1594538d3d85bbf64
alvarezmarlen <marlenjosefalvarez@gmail.com>
2025-12-04 14:22:34 +0100

    anexar fotos

---
+++ assets/js/productos.js
@@ -0,0 +1,41 @@
+
+const products = [
+    {
+        code: 'C002',
+        name: 'Collar dije Fresa con abalorios',
+        price: 8,
+        image: '/assets/img/CollarFrutilla-abalorios02.png'
+    },
+    {
+        code: 'C005',
+        name: 'Collar dije azul',
+        price: 12,
+        image: '/assets/img/AretesRojos-PerlasBlancas6.png'
+    },
+        {
+        code: 'C006',
+        name: 'Pulcera dije Fresa con hojas',
+        price: 12,
+        image: '/assets/img/PulceraFrutilla02.png'
+    }
+];
+
+
+const productList = document.getElementById('caja');
+
+products.forEach((product, index) => {
+    const col = document.createElement('div');
+    col.className = 'col';
+    col.innerHTML = `<div class="card" style="width: 18rem;">
+                        <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                            <p><strong>€ ${product.price}</strong></p>
+                        </div>
+                    </div>
+    
+    
+    `;
+    productList.appendChild(col);
+});
+
