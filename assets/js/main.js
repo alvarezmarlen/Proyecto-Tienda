dc1a99804f62d79317c07135744093aff5bae6e5
jorge <jorgecerecedatrabajo@gmail.com>
2025-12-05 12:15:32 +0100

    main modificado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,74 @@
 /* const articulosJSON =`[{"produtID": 0,
                         "categoria": ["zapatos","camisas","bolsos","pantalones"],
                         "productName": "playera negra con rayas blancas",
-                        "imagen": "img/zapatillaNegra.jpg",
+                        "imagen": img/zapatillaNegra.jpg,
                         "precio": 50,
                         "descripcion": "playera negra con rayas blancas, de la talla 41, con puntera reforzada",
                         "talla": "41",
-                        "stock": "true"
+                        "stock": true
                         },
+                        ] ` */
 
                         
 export const articulosJSON =[{"produtID": 0,
                         "categoria": "bolsos",
                         "productName": "Bolso Azul",
-                        "imagen": "img/bolsos/bolso-azul.jpg",
+                        "imagen": "../assets/img/bolsos/bolso-azul.jpg",
                         "precio": 35,
                         "descripcion": "Bolso azul con tres espacios",
                         "talla": "pequeno",
-                        "stock": "true"
+                        "stock": 2
                         },
 
                         {"produtID": 1,
                         "categoria": "bolsos",
                         "productName": "Bolso Marron",
-                        "imagen": "img/bolsos/bolso-marron.jpg",
+                        "imagen": "../assets/img/bolsos/bolso-marron.jpg",
                         "precio": 65,
                         "descripcion": "Bolso marron con asa color beige",
-                        "talla": "mediano",
-                        "stock": "true"
+                        "talla": "grande",
+                        "stock": 3
                         },
 
                         {"produtID": 2,
                         "categoria": "bolsos",
                         "productName": "Bolso Blanco",
-                        "imagen": "img/bolsos/bolso-media-luna-suave.jpg",
+                        "imagen": "../assets/img/bolsos/bolso-media-luna-suave.jpg",
                         "precio": 25,
                         "descripcion": "Bolso blanco con forma de media luna",
                         "talla": "mediano",
-                        "stock": "true"
+                        "stock": 1
                         },
 
                         {"produtID": 3,
                         "categoria": "bolsos",
                         "productName": "Mochila Azul",
-                        "imagen": "img/bolsos/mochila-azul.jpg",
+                        "imagen": "../assets/img/bolsos/mochila-azul.jpg",
                         "precio": 45,
                         "descripcion": "Mochila azul para ir al monte",
                         "talla": "mediano",
-                        "stock": "true"
+                        "stock": 1
                         },
 
                         {"produtID": 4,
                         "categoria": "bolsos",
                         "productName": "Mochila Oscura",
-                        "imagen": "img/bolsos/mochila-oscura.jpg",
+                        "imagen": "../assets/img/bolsos/mochila-oscura.jpg",
                         "precio": 30,
                         "descripcion": "Bolso gris de tres cremalleras",
                         "talla": "mediano",
-                        "stock": "true"
+                        "stock": 4
                         },
 
                         {"produtID": 5,
                         "categoria": "bolsos",
                         "productName": "troley Azul",
-                        "imagen": "img/bolsos/troley.jpg",
+                        "imagen": "../assets/img/bolsos/troley.jpg",
                         "precio": 70,
                         "descripcion": "Troley de color azul con 4 ruedas",
                         "talla": "mediano",
-                        "stock": "true"
+                        "stock": 3
                         },
-
 
                         
 
