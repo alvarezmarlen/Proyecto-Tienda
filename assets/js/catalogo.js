7c8abdf5090d44272822b7226687dd19282629cc
alvarezmarlen <marlenjosefalvarez@gmail.com>
2025-12-15 12:55:03 +0100

    carrtio arreglado lo de los articulos

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -36,54 +36,6 @@ articulosJSON.forEach((product, index) => {
     productList.appendChild(col);
 });
 
-// Función para agregar al carrito
-function agregarAlCarritoYRedirigir(product) {
-    // Obtener carrito actual
-// Intentamos obtener el carrito del localStorage. Si no existe, usamos "[]" para que JSON.parse no falle.
-    let carritoData = localStorage.getItem("carrito");
-    let carrito = JSON.parse(carritoData || "[]");
-    
-    let productoExiste = false;
-
-    for (let i = 0; i < carrito.length; i++) {
-        if (carrito[i].productID === product.produtID) {
-
-            // Si existe: Aumentar cantidad
-            carrito[i].cantidad = carrito[i].cantidad + 1;
-            
-            // Verificar que no supere el stock
-            if (carrito[i].cantidad > product.stock) {
-                carrito[i].cantidad = product.stock; // Limitar al stock máximo
-            }
-            
-            productoExiste = true;
-            break; // Salir del bucle for, ya encontramos y actualizamos el producto
-        }
-    }
-    
-  if (!productoExiste) {
-        // Si no se encontró el producto en el bucle, lo agregamos como una nueva compra
-        let Compra = {
-            productID: product.produtID,
-            categoria: product.categoria,
-            productName: product.productName,
-            imagen: product.imagen,
-            precio: product.precio,
-            descripcion: product.descripcion,
-            talla: product.talla,
-            stock: product.stock,
-            cantidad: 1 // La cantidad inicial es 1
-        };
-        carrito.push(Compra);
-    };
-    
-     // Guardar en localStorage
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    
-    // Redirigir al carrito
-    window.location.href = 'cart.html';
-}
-
 
 
 
