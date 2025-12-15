e4153def02da35ddd31aec3fffaacf316e6250e1
alvarezmarlen <marlenjosefalvarez@gmail.com>
2025-12-15 11:29:25 +0100

    agregar al carrito desde  las tarjetas

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -29,13 +29,64 @@ articulosJSON.forEach((product, index) => {
         localStorage.setItem('productoSeleccionado', JSON.stringify(product));
         window.location.href = 'detalle.html'; 
     });
-// ⬅️ ESTO EVITA que active el click de la tarjeta
+// Click en el botón → Agregar al carrito
     col.querySelector('.agregar').addEventListener('click', function(event) {
         event.stopPropagation();
+        agregarAlCarritoYRedirigir(product);
     })
 
     productList.appendChild(col);
 });
+
+// Función para agregar al carrito
+function agregarAlCarritoYRedirigir(product) {
+    // Obtener carrito actual
+// Intentamos obtener el carrito del localStorage. Si no existe, usamos "[]" para que JSON.parse no falle.
+    let carritoData = localStorage.getItem("carrito");
+    let carrito = JSON.parse(carritoData || "[]");
+    
+    let productoExiste = false;
+
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].productID === product.produtID) {
+
+            // Si existe: Aumentar cantidad
+            carrito[i].cantidad = carrito[i].cantidad + 1;
+            
+            // Verificar que no supere el stock
+            if (carrito[i].cantidad > product.stock) {
+                carrito[i].cantidad = product.stock; // Limitar al stock máximo
+            }
+            
+            productoExiste = true;
+            break; // Salir del bucle for, ya encontramos y actualizamos el producto
+        }
+    }
+    
+    // Crear objeto compra COMPLETO (como lo necesita car.js)
+if (!productoExiste) {
+        // Si no se encontró el producto en el bucle, lo agregamos como una nueva compra
+        let Compra = {
+            productID: product.produtID,
+            categoria: product.categoria,
+            productName: product.productName,
+            imagen: product.imagen,
+            precio: product.precio,
+            descripcion: product.descripcion,
+            talla: product.talla,
+            stock: product.stock,
+            cantidad: 1 // La cantidad inicial es 1
+        };
+        carrito.push(Compra);
+    };
+    
+     // Guardar en localStorage
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    
+    // Redirigir al carrito
+    window.location.href = 'cart.html';
+}
+
 
 
 
