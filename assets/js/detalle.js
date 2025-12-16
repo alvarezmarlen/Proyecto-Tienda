5d05f7654d7de50e70a9808d0aed1848ed9d00d7
Jorge Cereceda <jorgecerecedatrabajo@gmail.com>
2025-12-16 23:37:53 +0100

    arreglando mas errores

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -1,7 +1,9 @@
-//iniciamos numero de articulos comprados y el contenedor de la compra
+//iniciamos numero de articulos comprados
 let numero = 1;
 
-
+/*-------------------------------------------
+ponemos la informacion del producto
+----------------------------------------------*/
 // importarmos catalogo de main.js y el articulo seleccionado en productos de local storage
 import { articulosJSON } from './main.js';
 const articulo = JSON.parse(localStorage.getItem('productoSeleccionado'));
@@ -23,6 +25,9 @@ document.getElementById("cuadro").innerHTML = `
     <p> precio: ${articulo.precio} €</p>
     <p> tenemos ${articulo.stock} en stock</p>`
 
+/*-------------------------------------------------------
+Botones de sumar y restar
+----------------------------------------------------------*/
 // ponemos un escuchador a los botones de añadir y restar un producto
 const botonsumar = document.getElementById("botonsumar");
 botonsumar.addEventListener("click", sumar);
@@ -44,18 +49,21 @@ function restar() {
 document.getElementById("cantidad").innerHTML = numero;
 
 
-/* al pulsar añadir al carrito se muestra el cuadro de informacion y se guarda la orden en el objeto
-compra. las compras se almacenan en el array carrito, que guardamos en localstorage*/
+/* -------------------------------------------------------------------------------
+Al pulsar añadir al carrito se guarda la cantidad en el objeto compra y las compras se almacenan 
+en el array carrito, que guardamos en localstorage. Se muestra el cuadro de informacion.
+----------------------------------------------------------------------------------*/
 
 const botonabrirmodal = document.getElementById("añadir");
 botonabrirmodal.addEventListener("click", añadir);
 
 function añadir() {
     // asignar valores al objeto compra
-     const compra = {
+    const compra = {
         produtID: articulo.produtID,
         cantidad: numero,
-        precio: articulo.precio};
+        precio: articulo.precio
+    };
 
     // obtenemos el array carrito de localstorage, si no existe se crea uno vacio
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -63,13 +71,13 @@ function añadir() {
     //comprobamos si la compra esta en el carrito. 
     //Si existe buscamos la compra en el carrito, sumamos y comprobamos que no pasa el stock
 
-    if (carrito.some (obj => obj.produtID === compra.produtID)) {
-        const compraencarrito = carrito.find (orden => orden.produtID === compra.produtID);
+    if (carrito.some(obj => obj.produtID === compra.produtID)) {
+        const compraencarrito = carrito.find(orden => orden.produtID === compra.produtID);
 
         //sumar la cantidad
         compraencarrito.cantidad = compraencarrito.cantidad + numero;
-           document.getElementById("infocompra").innerText =`Tienes ${compraencarrito.cantidad}`;
-           
+        document.getElementById("infocompra").innerText = `Tienes ${compraencarrito.cantidad}`;
+
         //ver si supera el stock
         if (compraencarrito.cantidad > articulo.stock) {
             compraencarrito.cantidad = articulo.stock;
@@ -78,25 +86,26 @@ function añadir() {
         // guardar en localstorage
         localStorage.setItem("carrito", JSON.stringify(carrito));
 
-    // si no existe la añadimos y guardamos en local storage
+        // si no existe la añadimos y guardamos en local storage
     } else {
         carrito.push(compra);
         localStorage.setItem("carrito", JSON.stringify(carrito));
-        document.getElementById("infocompra").innerText =`Has añadido ${compra.cantidad}`;
+        document.getElementById("infocompra").innerText = `Has añadido ${compra.cantidad}`;
     }
-    
+
     //poner la foto
     document.getElementById("preview").src = `../assets/${articulo.imagen}`;
 
-    document.getElementById("infocompra2").innerText =`Has añadido ${articulo.productName}`;
-
+    document.getElementById("infocompra2").innerText = `Has añadido ${articulo.productName}`;
 
     // mostrar la ventana
     modal.style.display = "block";
-
 }
 
-/*cierra el cuadro para seguir comprando */
+/*-----------------------------------------
+botones del cuadro de informacion
+------------------------------------------*/
+//cierra el cuadro para seguir comprando
 const botoncerrarmodal = document.getElementById("seguircompra");
 botoncerrarmodal.addEventListener("click", cerrarmodal)
 
@@ -111,22 +120,22 @@ botonborrar.addEventListener("click", borrarcarrito);
 
 //cargamos pagina
 function borrarcarrito() {
-     window.location.href = 'cart.html';
+    window.location.href = 'cart.html';
 
 }
 
-/* REcomendacion de 4 articulos*/
-
-// Filtrar productos por categoría y ponerlos en array similares
+/* -----------------------------------------------
+Recomendacion de 4 articulos de la misma categoria
+--------------------------------------------------*/
 const categoriaBuscada = articulo.categoria;
 
+// Filtrar productos por categoría eliminando el actual y ponerlos en array similares
 const similares = articulosJSON.filter(filtrarcategoria);
 function filtrarcategoria(producto) {
     return producto.categoria == categoriaBuscada && producto.produtID != articulo.produtID;
 }
 
 //crea las tarjetas y la añade a contenedor2 
-
 
 for (let i = 0; i < 4; i++) {
     const tarjeta = document.createElement('div');
@@ -145,17 +154,56 @@ for (let i = 0; i < 4; i++) {
     
     `;
 
-     // Click en la tarjeta → ir al detalle
-    tarjeta.querySelector('.card-tarjeta').addEventListener('click', function() {
+    // Click en la tarjeta → ir al detalle
+    tarjeta.querySelector('.card-tarjeta').addEventListener('click', function () {
         localStorage.setItem('productoSeleccionado', JSON.stringify(similares[i]));
         window.location.href = 'detalle.html';
     });
 
     // Click en el botón → NO ir al detalle
-    tarjeta.querySelector('.agregar').addEventListener('click', function(event) {
+    tarjeta.querySelector('.agregar').addEventListener('click', function (event) {
         event.stopPropagation();
-        // Aquí tu compañero agrega el código del carrito
+
+        /* --------------------------------------------------------------------
+        copia del codigo anterior para agregar al carrito sin pasar por detalle
+        ----------------------------------------------------------------------*/
+
+        const compra = {
+            produtID: similares[i].produtID,
+            cantidad: 1,
+            precio: similares[i].precio
+        };
+
+        // obtenemos el array carrito de localstorage, si no existe se crea uno vacio
+        let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+        //comprobamos si la compra esta en el carrito. 
+        //Si existe buscamos la compra en el carrito, sumamos y comprobamos que no pasa el stock
+
+        if (carrito.some(obj => obj.produtID === compra.produtID)) {
+            const compraencarrito = carrito.find(orden => orden.produtID === compra.produtID);
+
+            //sumar la cantidad
+            compraencarrito.cantidad = compraencarrito.cantidad + numero;
+
+            //ver si supera el stock
+            if (compraencarrito.cantidad > articulo.stock) {
+                compraencarrito.cantidad = articulo.stock;
+            }
+            // guardar en localstorage
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+
+            // si no existe la añadimos y guardamos en local storage
+        } else {
+            carrito.push(compra);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        }
+        //-------------------------------------------------------------------
+
+        window.location.href = 'cart.html';
     });
 
     contenedor2.appendChild(tarjeta);
 }
+
+
